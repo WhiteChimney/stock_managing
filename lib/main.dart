b8c4faadf2e321e3090342372c42f54505ccb53b
WhiteChimney <white8chimney@gmail.com>
2023-10-29 00:51:20 +0800

    玩过了 ssh sftp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dartssh2/dartssh2.dart';
+import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
@@ -35,22 +36,125 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _returnState = 0;
+  String sshResult = 'not connected';
 
-  void tryServer() async {
-    final socket = await SSHSocket.connect('192.168.50.17', 22);
+  String sshReturn = 'not started';
+
+  late SSHClient client1;
+
+  Future<String> tryServer() async {
+    final socket = await SSHSocket.connect(
+      // '192.168.50.17', 
+      'lqcc301.ddns.net',
+      // '210.45.114.213',
+      // 22,
+      30109,
+      );
+
+    final client = SSHClient(
+      socket,
+      // username: 'noland',
+      // identities: [
+        // ...SSHKeyPair.fromPem(await File('C:/Users/79082/.ssh/id_ecdsa').readAsString())
+      // ],
+      username: 'root',
+      onPasswordRequest: () => 'zxh12345ZXH',
+    );
+
+    String? result;
+    try {
+      final uptime = await client.run('uptime');
+      result = utf8.decode(uptime);
+      client.close();
+      await client.done;
+    } catch (err) {
+      result = err.toString();
+    }
+
+
+    return result;
+  }
+
+  Future<List> connectServer() async {
+    final socket = await SSHSocket.connect(
+      '192.168.50.17', 22
+      );
 
     final client = SSHClient(
       socket,
       username: 'noland',
-      onPasswordRequest: () => 'zxh12345',
+      identities: [
+        ...SSHKeyPair.fromPem(await File('C:/Users/79082/.ssh/id_ecdsa').readAsString())
+      ],
     );
 
-    final uptime = await client.run('uptime');
-    print(utf8.decode(uptime));
+    String? result;
+    try {
+      await client.run('uptime');
+      result = 'successfully connected';
+    } catch (err) {
+      result = err.toString();
+    }
+    return [result,client];
+  }
 
-    client.close();
-    await client.done;
+  Future<String> sendCommand(String command, SSHClient client) async {
+    String ? result;
+    try {
+      final msg = await client.run(command);
+      result = utf8.decode(msg);
+    } catch (err) {
+      result = err.toString();
+    }
+    return result;
+  }
+  
+  Future<String> disconnectServer(SSHClient client) async {
+    String ? result;
+    try {
+      client.close();
+      await client.done;
+      result = 'successfully closed.';
+    } catch (err) {
+      result = err.toString();
+    }
+    return result;
+  }
+  
+  Future<String> receiveTestFile(SSHClient client) async {
+    String ? result;
+    try {
+      final sftp = await client.sftp();
+      final file = await sftp.open('/home/noland/test.txt');
+      final content = await file.readBytes();
+      result = latin1.decode(content);
+    } catch (err) {
+      result = err.toString();
+    }
+    return result;
+  }
+
+  Future<String> saveToLocal() async {
+    String ? result;
+    try {
+      final directory = await getApplicationCacheDirectory();
+      var file = File('${directory.path}/test.txt');
+      String content = sshReturn;
+      print(content);
+      file.writeAsString(content);
+      result = directory.path;
+    } catch (err) {
+      result = err.toString();
+    }
+    return result;
+  }
+
+  var myController = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
   }
 
   @override
@@ -68,15 +172,24 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       drawer: Drawer(
         child: ListView(
-          children: const [
+          children: [
             DrawerHeader(child: Column(
               children: [
                 Text('当前用户'),
                 Text('Noland'),
               ],
             ),),
-            ListTile(title: Text('设置'),),
-            ListTile(title: Text('关于'),),
+            ListTile(
+              title: Text('设置'), 
+              onTap: (){
+                print('用户想要设置');
+              },
+            ),
+            ListTile(title: Text('关于'),
+              onTap: (){
+                print('用户想要关于');
+              },
+            ),
           ],
         ),
       ),
@@ -84,8 +197,85 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:',),
-            ElevatedButton(onPressed: tryServer, child: Text('connect')),
+            Text(sshResult),
+            ElevatedButton(
+              onPressed: () async {
+                String res = await tryServer();
+                setState(() {
+                  sshResult = res;
+                });
+              },
+              child: Text('test')
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text(sshReturn),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter a command to be sent',
+                ),
+                controller: myController,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    var res = await connectServer();
+                    setState(() {
+                      sshReturn = res[0];
+                      client1 = res[1];
+                    });
+                  }, 
+                  child: Text('connect'),
+                ),
+                ElevatedButton(
+                  child: Text('disconnect'), 
+                  onPressed: () async {
+                    String res = await disconnectServer(client1);
+                    setState(() {
+                      sshReturn = res;
+                    });
+                  }, 
+                ),
+                ElevatedButton(
+                  child: Text('send'), 
+                  onPressed: () async {
+                    String res = await sendCommand(myController.text,client1);
+                    setState(() {
+                      sshReturn = res;
+                    });
+                  }, 
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  String res = await receiveTestFile(client1);
+                  setState(() {
+                    sshReturn = res;
+                  });
+                }, 
+                child: Text('receive')
+                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () async {
+                  String res = await saveToLocal();
+                  print(res);
+                }, 
+                child: Text('save')
+                ),
+            ),
           ],
         ),
       ),
