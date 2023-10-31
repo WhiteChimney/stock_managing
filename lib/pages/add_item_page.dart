560ac07648be15515c58c1d2f88c3c21031262cc
WhiteChimney <white8chimney@gmail.com>
2023-11-01 03:00:41 +0800

    修改ui中

--- lib/pages/add_item_page.dart
+++ lib/pages/add_item_page.dart
@@ -1,9 +1,11 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:convert';
 import 'package:path_provider/path_provider.dart';
 import 'package:stock_managing/tools/my_cameras.dart';
 import 'package:file_picker/file_picker.dart';
+// import 'package:json_annotation/json_annotation.dart;
 
 class AddItemPage extends StatefulWidget {
   @override
@@ -11,7 +13,22 @@ class AddItemPage extends StatefulWidget {
 }
 
 class _AddItemPageState extends State<AddItemPage> {
-  List<String> items = ['one', 'two', 'three', 'one', 'two', 'three', 'one'];
+  List<TextEditingController> nameControllers = [];
+  List<TextEditingController> contentControllers = [];
+
+  List<String> picPaths = [];
+  List<String> filePaths = [];
+
+  @override
+  void dispose() {
+    for (final controller in nameControllers) {
+      controller.dispose();
+    }
+    for (final controller in contentControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +37,7 @@ class _AddItemPageState extends State<AddItemPage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text('添加物品'),
           actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.check)),
+            IconButton(onPressed: saveItemInfo, icon: const Icon(Icons.check)),
           ],
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
@@ -38,31 +55,51 @@ class _AddItemPageState extends State<AddItemPage> {
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                      onPressed: getPictures, child: Text('添加照片')),
+                      onPressed: addPictures, child: Text('添加照片')),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: ElevatedButton(
-                      onPressed: readPicInfo, child: Text('添加附件')),
+                      onPressed: addOtherFiles, child: Text('添加附件')),
                 ),
               ],
             ),
           ),
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: picPaths.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: generatePictureWidget(picPaths[index]),
+                  );
+              }),
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              // scrollDirection: Axis.horizontal,
+              itemCount: filePaths.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: generateFilesWidget(filePaths[index]),
+                  );
+              }),
+          ),
           Expanded(
             child: ListView.builder(
-                itemCount: items.length,
+                itemCount: nameControllers.length,
                 itemBuilder: (context, index) {
-                  final item = items[index];
-                  return GestureDetector(
-                    child: Padding(
+                  return Padding(
                       padding: const EdgeInsets.all(18.0),
-                      child: Text(item),
-                    ),
-                    onTap: () {
-                      items.removeAt(index);
-                      setState(() {});
-                    },
-                  );
+                      child: 
+                      generateEntryWidget(
+                        nameControllers[index],
+                        contentControllers[index]),
+                    );
                 }),
           ),
         ],
@@ -75,12 +112,72 @@ class _AddItemPageState extends State<AddItemPage> {
     );
   }
 
+  Column generateEntryWidget(
+    TextEditingController nameController, 
+    TextEditingController contentController) {
+    return 
+      Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                icon: Icon(Icons.bookmark),
+                border: UnderlineInputBorder(),
+                hintText: '条目名称',
+              ),
+              controller: nameController,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              minLines: 2,
+              maxLines: 5,
+              decoration: InputDecoration(
+                icon: Icon(Icons.assignment_outlined),
+                border: OutlineInputBorder(),
+                hintText: '条目内容',
+              ),
+              controller: contentController,
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              nameController.dispose();
+              nameControllers.remove(nameController);
+              contentController.dispose();
+              contentControllers.remove(contentController);
+              setState(() {});
+            },
+          )
+        ],
+      );
+  }
+
   void addEntry() {
-    items.add('ooo');
+    final nameController = TextEditingController();
+    final contentController = TextEditingController();
+    nameControllers.add(nameController);
+    contentControllers.add(contentController);
     setState(() {});
   }
 
-  void getPictures() async {
+  void saveItemInfo() async {
+    Map<String,dynamic> json = {};
+    for (int index = 0; index < nameControllers.length; index++) {
+      json[nameControllers[index].text] = contentControllers[index].text;
+    }
+
+    final directory = await getApplicationCacheDirectory();
+    var fWrite = File('${directory.path}/json_test.json');
+    var contents = jsonEncode(json);
+    fWrite.writeAsString(contents);
+  }
+
+  void addPictures() async {
     if (Platform.isIOS || Platform.isAndroid) {
       CameraDescription camera = await getCamera();
       if (!context.mounted) return;
@@ -90,51 +187,52 @@ class _AddItemPageState extends State<AddItemPage> {
       FilePickerResult? result = await FilePicker.platform.pickFiles();
 
       if (result != null) {
-        File file = File(result.files.single.path.toString());
-        print(file.path);
-      } else {
-        // User canceled the picker
-        print('cancelled.');
-      }
+        picPaths.add(result.files.single.path.toString());
+        print(picPaths);
+        setState(() {});
+      } 
     }
   }
 
-  void readPicInfo() async {
-    if (Platform.isIOS || Platform.isAndroid) {
-      late String fileName;
-      try {
-        var filePath = await getApplicationCacheDirectory();
-        fileName =
-            '${filePath.path}/CAP_D6C6C735-90E7-4E84-A62A-DEF1A1598EF8.jpg';
-        var file = File(fileName);
-        print(await file.exists());
-        var image = Image.file(
-          file,
-          width: 100,
-          height: 200,
-        );
-        print('The image fullname is ${fileName}');
-        print(image);
-        print('The image size is ${image.width} x ${image.height}');
-      } catch (err) {
-        print(err);
-        return;
-      }
-
-      if (!context.mounted) return;
-
-      // If the picture was taken, display it on a new screen.
-      await Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => DisplayPictureScreen(
-            // Pass the automatically generated path to
-            // the DisplayPictureScreen widget.
-            imagePath: fileName,
+  Column generatePictureWidget(String picPath) {
+    return Column(
+      children: [
+        Image.file(File(picPath),
+          height: 144,
+          errorBuilder:
+            (BuildContext context, Object exception, StackTrace? stackTrace) {
+              print('error occurred');
+              return Image(image: AssetImage('assets/images/image_loading_failed.png'));
+            },
           ),
-        ),
-      );
-    } else {
-      print('Sorry, not supported. ');
+        IconButton(onPressed: (){
+          picPaths.remove(picPath);
+          setState(() {});
+        }, icon: Icon(Icons.delete),),
+      ],
+    );
+  }
+
+  void addOtherFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      filePaths.add(result.files.single.path.toString());
+      print(filePaths);
+      setState(() {});
     }
   }
+
+  Row generateFilesWidget(String filePath) {
+    return Row(
+      children: [
+        IconButton(onPressed: (){
+          filePaths.remove(filePath);
+          setState(() {});
+        }, icon: Icon(Icons.delete),),
+        SizedBox(child: Text(filePath), width: MediaQuery.of(context).size.width-56,),
+      ],
+    );
+  }
+
 }
